4c8cf22203047ac16a542ba16c8336a4ae405db4
gpetrini <gpetrinidasilveira@gmail.com>
2018-08-29 14:51:35 -0300

    Adicionada funções.

--- R/FazTexto.AccMes.R
+++ R/FazTexto.AccMes.R
@@ -1,4 +1,12 @@
-#'
+#' @title Faz Texto Acumulado Mes
+#' @aliases FazTexto.AccMes
+#' @author Arthur Welle
+#' @import
+#' @description Gera texto com taxa acumulada em m meses
+#' @return Texto
+#' @param x Serie a ser calculada a taxa
+#' @param digitos Numero de casas decimais. Por padrao, 2
+#' @param meses Numero de meses para acumular. Por padrao, 12 meses
 
 FazTexto.AccMes <- function (x, digitos = 2, meses = 12)
 {
