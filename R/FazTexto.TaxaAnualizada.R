be4b344feba58557e2b45304ad85240dd1ed6e8b
gpetrini <gpetrinidasilveira@gmail.com>
2018-09-11 10:50:52 -0300

    Automatizada detecção da periodicidade da taxa anualizada.

--- R/FazTexto.TaxaAnualizada.R
+++ R/FazTexto.TaxaAnualizada.R
@@ -5,16 +5,37 @@
 #' @export
 #' @param x Serie para calcular taxa anualizada
 #' @param digitos Numero de casas decimais
+#' @param tempo Se mensal 12, trimestral 4. Se for xts, ts ou zoo, calcula automaticamente.
 #' @param texto Se TRUE retorna caractere
 
 
-FazTexto.TaxaAnualizada <- function (x, digitos = 2, texto = TRUE)
+FazTexto.TaxaAnualizada <- function (x, digitos = 2, texto = TRUE, tempo = 12)
 {
+    if (class(x) == "xts" | class(x) == "ts" | class(x) == "zoo") {
+        if ((periodicity(x) %in% "monthly")[6] == TRUE) {
+            k <- c(1:length(x))
+            for (i in 0:length(x)) {
+                k[i] <- (1 + x[i]/100)^12
+            }
+            k <- (k - 1) * 100
+        } else if ((periodicity(x) %in% "quarterly")[6] == TRUE) {
+            k <- c(1:length(x))
+            for (i in 0:length(x)) {
+                k[i] <- (1 + x[i]/100)^4
+            }
+            k <- (k - 1) * 100
+        } else if ((periodicity(x) %in% "yearly")[6] == TRUE) {
+            warning("Série já anual")
+        }  else {
+            warning("Rever Série")
+        }
+    } else {
     k <- c(1:length(x))
     for (i in 0:length(x)) {
-        k[i] <- (1 + x[i]/100)^12
+        k[i] <- (1 + x[i]/100)^tempo
     }
     k <- (k - 1) * 100
+    }
     if (texto == TRUE) {
     Taxa_Anuali <- format(k[length(x)],
                           big.mark = ".",
@@ -22,6 +43,10 @@ FazTexto.TaxaAnualizada <- function (x, digitos = 2, texto = TRUE)
                           digits = digitos)
     return(Taxa_Anuali)
     } else {
+        if (class(x) == "xts" | class(x) == "ts" | class(x) == "zoo") {
+        return(coredata(k[length(x)]))
+        } else {
         return(k[length(x)])
+        }
     }
 }
